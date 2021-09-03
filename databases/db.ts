e2e94472b148232dfbe098d30abeea4cab523574
adt-hgkim <hgkim@gnmpm.com>
2021-09-03 12:45:22 +0900

    🎉 init denodb

---
+++ databases/db.ts
@@ -0,0 +1,9 @@
+import { Database, SQLite3Connector } from "../library/denodb.ts"
+
+const connector = new SQLite3Connector({
+  filepath: "./database.sqlite",
+})
+
+const db = new Database(connector)
+
+export default db
